d7305d3e7d1b9ba231dc4e3f7f63ec48b62c48a3
Roman Uvarov <uvarovrs40@gmail.com>
2022-01-17 18:57:56 +0300

    added fullfillable chars iterator

--- src/interpreter/string_char.rs
+++ src/interpreter/string_char.rs
@@ -1,5 +1,8 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
 pub struct CharsIter<'code> {
-	ch_iter: std::iter::Peekable<std::str::Chars<'code>>,
+	ch_iter: Peekable<Chars<'code>>,
 	pos: usize,
 }
 
@@ -118,5 +121,92 @@ impl std::fmt::Display for CharKind {
 			},
 			CharKind::Invalid (ch) => write!(f, "{}", ch),
 		}
+	}
+}
+
+//#[test]
+mod tests {
+	use std::iter::Peekable;
+	use std::str::Chars;
+	use std::collections::VecDeque;
+	use super::CharKind;
+	
+	struct ChIt {
+		strings_queue: VecDeque<String>,
+		chars_queue: VecDeque<char>,
+	}
+	
+	impl ChIt {
+		fn new() -> Self {
+			Self {
+				strings_queue: VecDeque::<String>::new(),
+				chars_queue: VecDeque::<char>::new(),
+			}
+		}
+		
+		fn push_string(&mut self, string: String) {
+			self.strings_queue.push_back(string);
+		}
+	}
+	
+	impl Iterator for ChIt {
+		type Item = char;
+		
+		fn next(&mut self) -> Option<Self::Item> {			
+			while self.strings_queue.len() > 0 && self.chars_queue.is_empty() {
+				let cur_string = self.strings_queue.pop_front().unwrap();
+				
+				println!("start '{}'", cur_string);
+				
+				for ch in cur_string.chars() {
+					self.chars_queue.push_back(ch);
+				}
+			}
+			
+			self.chars_queue.pop_front()
+		}
+	}
+	
+	#[test]
+	fn try_ch_it() {
+		let mut ch_it = ChIt::new();
+		use std::collections::VecDeque;
+		use std::iter::FromIterator;
+		
+		assert_eq!(ch_it.next(), None);
+		
+		// try 1 strings
+		ch_it.push_string("abc".to_string());
+		assert_eq!(ch_it.strings_queue, VecDeque::from(vec![
+			"abc".to_string(),
+		]));
+		
+		assert_eq!(ch_it.next(), Some('a'));
+		assert_eq!(ch_it.next(), Some('b'));
+		assert_eq!(ch_it.next(), Some('c'));
+		assert_eq!(ch_it.next(), None);
+		assert_eq!(ch_it.strings_queue, VecDeque::new());
+		
+		// try 2 strings
+		ch_it.push_string("def".to_string());
+		ch_it.push_string("ghi".to_string());
+		assert_eq!(ch_it.strings_queue, VecDeque::from(vec![
+			"def".to_string(),
+			"ghi".to_string(),
+		]));
+		
+		assert_eq!(ch_it.next(), Some('d'));
+		assert_eq!(ch_it.next(), Some('e'));
+		assert_eq!(ch_it.next(), Some('f'));
+		assert_eq!(ch_it.next(), Some('g'));
+		assert_eq!(ch_it.next(), Some('h'));
+		assert_eq!(ch_it.next(), Some('i'));
+		assert_eq!(ch_it.next(), None);		
+		assert_eq!(ch_it.strings_queue, VecDeque::new());
+		
+		// try empty string
+		ch_it.push_string("".to_string());
+		assert_eq!(ch_it.next(), None);		
+		assert_eq!(ch_it.strings_queue, VecDeque::new());
 	}
 }
